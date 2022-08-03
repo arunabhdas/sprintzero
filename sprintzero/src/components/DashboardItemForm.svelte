b5609c5f4f039917d679d78b2f6fa89bf44f81c6
Arunabh Das <arunabhdas@gmail.com>
2022-08-03 01:59:08 -0400

    Added button

--- sprintzero/src/components/DashboardItemForm.svelte
+++ sprintzero/src/components/DashboardItemForm.svelte
@@ -1,14 +1,31 @@
 <main class="flex-1">
-    <div class="py-6">
-      <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-        <h1 class="text-2xl font-semibold text-gray-900">Dashboard</h1>
-      </div>
-      <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-        <!-- Replace with your content -->
-        <div class="py-4">
-          <div class="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
-        </div>
-        <!-- /End replace -->
-      </div>
-    </div>
-  </main>
+	<div class="py-6">
+		<div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+			<h1 class="text-2xl font-semibold text-gray-900">Dashboard</h1>
+		</div>
+		<div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+			<!-- Replace with your content -->
+			<form>
+				<div>
+					<label for="dashboarditem" />
+					<input
+						type="text"
+						name="dashboarditem"
+						placeholder="Add a new item to the dashboard"
+						class="border border-gray-300 rounded-lg p-2"
+					/>
+				</div>
+				<button
+					type="submit"
+					class="bg-sky-500 hover:bg-sky-700 text-white font-bold py-2 px-4 rounded-lg"
+				>
+					Add
+				</button>
+			</form>
+			<div class="py-4">
+				<div class="border-4 border-dashed border-gray-200 rounded-lg h-96" />
+			</div>
+			<!-- /End replace -->
+		</div>
+	</div>
+</main>
