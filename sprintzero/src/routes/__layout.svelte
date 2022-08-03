c3fa94318babf92cdd3a64d8309b48f8feb0c63c
Arunabh Das <arunabhdas@gmail.com>
2022-08-03 01:15:54 -0400

    Improved layout of Dashboard

--- sprintzero/src/routes/__layout.svelte
+++ sprintzero/src/routes/__layout.svelte
@@ -1,6 +1,12 @@
 <script>
-    import "../app.css";
-  </script>
- <div class="container mx-auto">
-  <slot></slot>
- </div> 
+	import '../app.css';
+</script>
+
+<!-- svelte-ignore a11y-missing-attribute -->
+<html class="h-full bg-gray-100">
+	<body class="h-full">
+		<div class="md:container md:mx-auto">
+			<slot />
+		</div>
+	</body>
+</html>
